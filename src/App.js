76fb75bdb9c78ef9935d5f24bbee776145633d77
Grey Pegram <dpegram85@gmail.com>
2024-07-20 13:52:10 -0400

    Completed Labs

--- src/App.js
+++ src/App.js
@@ -34,4 +34,7 @@ function App() {
 
 export default App;
 
-// Note: We placed our call to navigate within our useEffect because we want to navigate our user after they've successfully logged in or out. By placing the state that dictates whether or not a user is logged in or out within the dependency array of our useEffect, we can programmatically navigate our user whenever a change in state occurs. This approach means we only call navigate once our state has updated. You don't always have to put navigate inside of a useEffect, but it makes sense to do so in this case.
+// Note: We placed our call to navigate within our useEffect because we want to navigate our user after they've successfully logged in or out. 
+// By placing the state that dictates whether or not a user is logged in or out within the dependency array of our useEffect, we can 
+// programmatically navigate our user whenever a change in state occurs. This approach means we only call navigate once our state has updated. 
+// You don't always have to put navigate inside of a useEffect, but it makes sense to do so in this case.
